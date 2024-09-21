eb0183bf4e64cbb63d5240609f9c785499734963
¨Karen <karen.saavedra05@inacapmail.cl>
2024-09-21 16:03:57 -0300

    Inicio Ejercicios 2 al 6 M5

---
+++ app/src/main/java/cl/example/ejerciciosm5_2al6/MainActivity.kt
@@ -0,0 +1,47 @@
+package cl.example.ejerciciosm5_2al6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cl.example.ejerciciosm5_2al6.ui.theme.EjerciciosM5_2al6Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EjerciciosM5_2al6Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EjerciciosM5_2al6Theme {
+        Greeting("Android")
+    }
+}
